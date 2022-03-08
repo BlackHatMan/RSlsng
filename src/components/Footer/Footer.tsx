13e7476da28916415a5715097aa9810bff894e98
BlackHatMan <38476233+BlackHatMan@users.noreply.github.com>
2022-03-09 01:45:23 +0300

    fix: style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import Button from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 import './Footer.css';
 
 const team = [
@@ -24,43 +23,47 @@ const team = [
     },
 ];
 
-export const Footer = () => (
-    <div className="footer">
-        <div className="wrapper wrapper__footer">
-            <div className="git_link">
-                <a
-                    href="https://github.com/serguntchik/rslang-team-serguntchik/tree/develop"
-                    target="_blank"
-                    rel="noreferrer"
-                    style={{ padding: '5px' }}
-                    className="footer_link"
-                >
-                    Github
-                </a>
-                © 2022
-                <a
-                    className="footer_link"
-                    href="https://https://rs.school/"
-                    target="_blank"
-                    rel="noreferrer"
-                    style={{ padding: '5px' }}
-                >
-                    RS School
-                </a>
-            </div>
-            <div className="git-link">
-                {team.map((member) => (
-                    <Button
-                        key={member.name}
-                        href={member.github}
+export const Footer = () => {
+    const matches = useMediaQuery('(max-width: 620px)');
+
+    return (
+        <div className="footer">
+            <div className="wrapper__footer">
+                <div className="git_link">
+                    <a
+                        href="https://github.com/serguntchik/rslang-team-serguntchik/tree/develop"
                         target="_blank"
-                        sx={{ my: 2, color: 'white' }}
-                        startIcon={<GitHubIcon />}
+                        rel="noreferrer"
+                        style={{ padding: '5px' }}
+                        className="footer_link"
                     >
-                        {member.name}
-                    </Button>
-                ))}
+                        Github
+                    </a>
+                    © 2022
+                    <a
+                        className="footer_link"
+                        href="https://https://rs.school/"
+                        target="_blank"
+                        rel="noreferrer"
+                        style={{ padding: '5px' }}
+                    >
+                        RS School
+                    </a>
+                </div>
+                <div className="git-link">
+                    {team.map((member) => (
+                        <Button
+                            key={member.name}
+                            href={member.github}
+                            target="_blank"
+                            sx={{ my: 2, color: 'white', fontSize: matches ? '0.7rem' : '1rem' }}
+                            startIcon={<GitHubIcon />}
+                        >
+                            {member.name}
+                        </Button>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
